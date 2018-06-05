208147cc7bc5367ba00b8c6eae04983fc7a5e95a
persia sarram <kittycuppers01@gmail.com>
2018-06-05 16:39:57 +0000

    automatically backed up by learn

--- methods_arguments.rb
+++ methods_arguments.rb
@@ -1,5 +1,6 @@
 #def greeting
  # name = "persia"
  # puts "my name is #{name} "
-end
-greeting
+#end
+#greeting
+#method called greeting.spits out what ever name i assign. method has to be called at the end
