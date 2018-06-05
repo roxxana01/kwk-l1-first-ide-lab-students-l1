c3b5fc96cf5d0721aec274e3188327b1c0ee254e
persia sarram <kittycuppers01@gmail.com>
2018-06-05 16:50:02 +0000

    automatically backed up by learn

--- methods_arguments.rb
+++ methods_arguments.rb
@@ -3,4 +3,18 @@
  # puts "my name is #{name} "
 #end
 #greeting
-#method called greeting.spits out what ever name i assign. method has to be called at the end
+#method called greeting.spits out what ever name i assign. method has to be called at the end in order to run 
+
+#greeting ("hey" ,"michelle" ,"dayi","morning")
+#greeting
+#Hey,Dayi,im michelle,hows your morning
+
+
+def greeting (local_greeting, your_name, my_name , local_time)
+  puts "#{local_greeting} #{your_name}, im #{my_name}, hows your #{local_time}?"
+end
+greeting("hey" ,"michelle" ,"dayi","morning")
+
+
+
+
